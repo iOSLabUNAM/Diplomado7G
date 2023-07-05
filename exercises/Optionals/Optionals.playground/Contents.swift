5adfbfcbcc53d7db98016fa6580ad405451415fc
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2023-07-04 18:02:10 -0600

    Exercise: Optionals

---
+++ exercises/Optionals/Optionals.playground/Contents.swift
@@ -0,0 +1,57 @@
+import Foundation
+import XCTest
+
+class OptionalsTests: XCTestCase {
+    func testOptional() {
+        var optionalInt: Int?
+        var optionalStr: String?
+        
+        XCTAssertNil(optionalInt)
+        XCTAssertNil(optionalStr)
+    }
+    
+    func testAssignOptional() {
+        let value: Int? = 42
+        
+        XCTAssertNotNil(value)
+        // TODO: make it pass this test
+        XCTAssertEqual(Int(42), value)
+    }
+    
+    func testUnsafeUnwrap(){
+        var value: Int?
+        value = 42
+        
+        XCTAssertEqual(42, value!)
+    }
+    
+    func testSafeUnwrapWithDefault() {
+        var value: Int?
+        value = 42
+        
+        XCTAssertEqual(42, value ?? 12)
+        value = nil
+        XCTAssertEqual(12, value ?? 12)
+    }
+    
+    func testSafeUnwrapWithIf(){
+        var value: Int?
+        value = 42
+        
+        if let unwrapedValue = value {
+            XCTAssertEqual(42, unwrapedValue)
+        } else {
+            XCTAssert(false, "Unexpected nil value")
+        }
+    }
+
+    func testGuardClauseExercise() {
+        var value: Int?
+        value = 42
+
+        XCTAssertTrue(evaluateOptional(value: value))
+        value = nil
+        XCTAssertFalse(evaluateOptional(value: value))
+}
+
+OptionalsTests.defaultTestSuite.run()
---
+++ exercises/Optionals/README.md
@@ -0,0 +1,3 @@
+## Optionals
+
+Given a array with optional values and pass it through a function that evaluates the input and returns true if a value is optional using a guard clause.
